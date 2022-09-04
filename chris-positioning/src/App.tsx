62dfebc73b454b10b80a8b4d2e207102174c075d
Davydx7 <oludave0511@gmail.com>
2022-09-04 19:23:19 +0100

    eslint prettier

--- chris-positioning/src/App.tsx
+++ chris-positioning/src/App.tsx
@@ -1,29 +1,33 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from 'react';
+import './App.css';
 
 function App() {
-  const [up, setUp] = useState(0)
-  const [right, setRight] = useState(0)
-  const [down, setDown] = useState(0)
-  const [left, setLeft] = useState(0)
+  const [up, setUp] = useState(0);
+  const [right, setRight] = useState(0);
+  const [down, setDown] = useState(0);
+  const [left, setLeft] = useState(0);
 
   return (
     <div className="App">
-      <div className='outerDiv'>
-        <div className='innerDiv' style={{height: `${up}px`, width: `${right}px`  }}></div>
+      <div className="column-1" />
+      <div className="column-2">
+        <div className="outerDiv">
+          <div className="innerDiv" style={{ height: `${up}px`, width: `${right}px` }} />
+        </div>
       </div>
+      <div className="column-3" />
       <div className="card">
         up
-        <input type='number' onChange={(e)=>setUp(+e.currentTarget.value)}/>
+        <input type="number" onChange={(e) => setUp(+e.currentTarget.value)} />
         right
-        <input type='number' onChange={(e)=>setRight(+e.currentTarget.value)} />
+        <input type="number" onChange={(e) => setRight(+e.currentTarget.value)} />
         down
-        <input type='number' onChange={(e)=>setDown(+e.currentTarget.value)} />
+        <input type="number" onChange={(e) => setDown(+e.currentTarget.value)} />
         left
-        <input type='number' onChange={(e)=>setLeft(+e.currentTarget.value)} />
+        <input type="number" onChange={(e) => setLeft(+e.currentTarget.value)} />
       </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
