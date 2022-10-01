d17385f9b13819f4c762cfc791ec48ff79453810
Davydx7 <oludave0511@gmail.com>
2022-10-01 14:43:59 +0100

    repo factory function init

---
+++ chris-git-view/src/utilities/repo.ts
@@ -0,0 +1,12 @@
+function repo (): object {
+	const createCommit = undefined;
+	const repoObject = {
+		file: 'strings',
+		createCommit: () => 'void',
+		createPullRequest: () => 'void'
+	}
+
+	return repoObject
+}
+
+export default repo;
