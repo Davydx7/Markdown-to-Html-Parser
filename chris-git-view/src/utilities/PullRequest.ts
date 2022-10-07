60f84c33726d1346b6d1b52a1d5c69e9f1896bc0
Davydx7 <69201432+Davydx7@users.noreply.github.com>
2022-10-07 21:24:21 +0100

    fleshing out constructors

---
+++ chris-git-view/src/utilities/PullRequest.ts
@@ -0,0 +1,16 @@
+class GitRepo {
+	public commits: object[]
+	public branches: object[]
+	public currentBranch: object
+	public author: string
+
+	constructor () {
+		this.author = ''
+		this.branches= []
+		this.currentBranch= {}
+		this.commits= []
+	}
+
+}
+
+export default GitRepo;
