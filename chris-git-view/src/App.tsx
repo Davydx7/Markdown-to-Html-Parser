b8fefa4b910e60626e81a87b5a4e705b4e31cf49
David Olufemi <oludave0511@gmail.com>
2022-10-09 21:41:53 +0100

    graphing works!

--- chris-git-view/src/App.tsx
+++ chris-git-view/src/App.tsx
@@ -3,28 +3,31 @@ import reactLogo from './assets/react.svg';
 import './App.css';
 import { Gitgraph } from '@gitgraph/react';
 import { createGitgraph } from '@gitgraph/js';
+import { useRepoStore } from './controllers/repoStore';
 
 function App(): JSX.Element {
   const [count, setCount] = useState(0);
-  const ref = useRef(null);
+  const ref = useRef<HTMLDivElement>(null);
+  const { createRepo, createBranch, createCommit, setRefDiv } = useRepoStore((state) => state);
 
   useEffect(() => {
     if (ref.current !== null) {
-      const gitgraph = createGitgraph(ref.current);
+      setRefDiv!(ref.current);
+      // const gitgraph = createGitgraph(ref.current);
 
-      const master = gitgraph.branch('master');
-      master.commit('Initial commit');
+      // const master = gitgraph.branch('master');
+      // master.commit('Initial commit');
 
-      const develop = gitgraph.branch('develop');
-      develop.commit('Add TypeScript');
+      // const develop = gitgraph.branch('develop');
+      // develop.commit('Add TypeScript');
 
-      const aFeature = gitgraph.branch('a-feature');
-      aFeature.commit('Make it work').commit('Make it right').commit('Make it fast');
+      // const aFeature = gitgraph.branch('a-feature');
+      // aFeature.commit('Make it work').commit('Make it right').commit('Make it fast');
 
-      develop.merge(aFeature);
-      develop.commit('Prepare v1');
+      // develop.merge(aFeature);
+      // develop.commit('Prepare v1');
 
-      master.merge(develop).tag('v1.0.0');
+      // master.merge(develop).tag('v1.0.0');
     }
   }, []);
 
@@ -46,6 +49,11 @@ function App(): JSX.Element {
         <a href="https://reactjs.org" target="_blank" rel="noreferrer">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
+      </div>
+      <div>
+        <button onClick={() => createRepo('myRepo')}>create repo</button>
+        <button onClick={() => createBranch('myBranch')}>create branch</button>
+        <button onClick={() => createCommit('commit')}>commit</button>
       </div>
       <div ref={ref}></div>
       <Gitgraph>{func as any}</Gitgraph>
--- chris-git-view/src/controllers/repoStore.ts
+++ chris-git-view/src/controllers/repoStore.ts
@@ -1,29 +1,45 @@
 import create from 'zustand';
 
+import { createGitgraph } from '@gitgraph/js';
+
 import GitRepo from '../gitConstructors/GitRepo';
 import GitBranch from '../gitConstructors/GitBranch';
 
 interface GitRepoStore {
   gitRepo: GitRepo | null;
   gitBranch: GitBranch | null;
+  repoGraph: any;
+  branchGraph: any;
+
+  refDiv?: HTMLDivElement | null;
+  setRefDiv?: (ref: HTMLDivElement) => void;
   createRepo: (name: string) => void;
-  createBranch: (branchName: string, gitRepo: GitRepo) => void;
+  createBranch: (branchName: string) => void;
   createCommit: (branchName: string) => void;
 }
 
 export const useRepoStore = create<GitRepoStore>((set, get) => ({
   gitRepo: null,
   gitBranch: null,
+  refDiv: null,
+
+  repoGraph: '',
+  branchGraph: '',
 
   createRepo: (name: string) => {
     const repo = new GitRepo(name);
+
+    const repoGraph = createGitgraph(get().refDiv!);
     set({ gitRepo: repo });
+    set({ repoGraph });
   },
 
   createBranch: (name: string) => {
     if (get().gitRepo !== null) {
       const branch = get().gitRepo!.createBranch(name);
+      const branchGraph = get().repoGraph.branch(name);
       set({ gitBranch: branch });
+      set({ branchGraph });
     }
   },
 
@@ -31,6 +47,12 @@ export const useRepoStore = create<GitRepoStore>((set, get) => ({
     if (get().gitBranch !== null) {
       const updatedBranch = get().gitBranch!.createCommit(commitName);
       set({ gitBranch: updatedBranch });
+
+      get().branchGraph.commit(commitName);
     }
+  },
+
+  setRefDiv(ref) {
+    set({ refDiv: ref });
   }
 }));
