fdd2b2ff2c24d533ac4adefe283327cb73fc4fee
David Olufemi <oludave0511@gmail.com>
2022-10-21 16:51:13 +0100

    sidebar items

--- chris-git-view/src/components/panels/Sidebar/index.tsx
+++ chris-git-view/src/components/panels/Sidebar/index.tsx
@@ -1,6 +1,27 @@
 import './sidebar.scss';
 
 const Sidebar = () => {
-  return <nav className="sidebar">Sidebar</nav>;
+  const screens = [
+    'Source',
+    'Conmits',
+    'Branches',
+    'Pull Requests',
+    'settings',
+    'Diff Viewer',
+    'Graph Viewer'
+  ];
+
+  return (
+    <nav className="sidebar">
+      <h1>Git View</h1>
+      <ul>
+        {screens.map((screen) => (
+          <li key={screen} className="sidebar__item">
+            {screen}
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
 };
 export default Sidebar;
