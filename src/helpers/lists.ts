4e264bead71b5e194d6b321d78743d68df58a89c
Davydx7 <oludave0511@gmail.com>
2024-12-06 17:52:23 +0100

    refactor list.ts

--- src/helpers/lists.ts
+++ src/helpers/lists.ts
@@ -9,49 +9,59 @@ function lists(match: string) {
     leadingSpaces.push(line.length - line.trimStart().length);
     const pushIn = leadingSpaces[i] - leadingSpaces[i - 1]; // leading space diff
 
-    const isOl = !!line.match(/^ *\d/);
+    const isOl = /^ *\d/.test(line);
     const lineNum = isOl ? `start=${line.match(/\d+/)![0]}` : '';
     const el = isOl ? 'ol' : 'ul';
 
     // task checkbox
-    const isChecked = !!line.match(/^ *(?:\d+\.|[-+*]) +\[x\] /i);
-    const isUnchecked = !!line.match(/^ *(?:\d+\.|[-+*]) +\[ \] /i);
+    const isChecked = /^ *(?:\d+\.|[-+*]) +\[x\] /i.test(line);
+    const isUnchecked = /^ *(?:\d+\.|[-+*]) +\[ \] /i.test(line);
+    const isTask = isChecked || isUnchecked;
 
     // checbox
-    const input =
-      isChecked || isUnchecked
-        ? `<input type="checkbox" disabled class="item-checkbox" ${isChecked ? 'checked' : ''}>`
-        : '';
+    const input = isTask
+      ? `<input type="checkbox" disabled class="item-checkbox" ${isChecked ? 'checked' : ''}>`
+      : '';
 
-    const classBox = isChecked || isUnchecked ? 'class="checkbox-item"' : '';
+    const classBox = isTask ? 'class="checkbox-item"' : '';
 
     // regex to trim off markdown syntax from list text
-    const reg = /^ *(?:\d+\.|[-+*]) +(?:\[[ x]\] +)?/i;
+    const markDownRegex = /^ *(?:\d+\.|[-+*]) +(?:\[[ x]\] +)?/i;
 
     if (Number.isNaN(pushIn)) {
       // firs line
       nestDepths.push([el, leadingSpaces[i - 1]]);
       result += `<${el} class='top' ${lineNum}>\n<li ${classBox}>${input} ${line
-        .replace(reg, '')
+        .replace(markDownRegex, '')
         .trim()}`;
-    } else if (pushIn === 0 || pushIn === 1) {
+      return;
+    }
+
+    if (pushIn === 0 || pushIn === 1) {
       // no nest change
-      result += `</li>\n<li ${classBox}>${input} ${line.replace(reg, '').trim()}`;
-    } else if (pushIn > 1) {
+      result += `</li>\n<li ${classBox}>${input} ${line.replace(markDownRegex, '').trim()}`;
+      return;
+    }
+
+    if (pushIn > 1) {
       // nestIn
       nestDepths.push([el, leadingSpaces[i - 1]]);
 
-      result += `\n<${el} ${lineNum}>\n<li ${classBox}>${input} ${line.replace(reg, '').trim()}`;
-    } else if (pushIn < 0) {
-      // nestOut
-      // pop out of nest as many times as we need to
-      while (nestDepths.length > 1 && leadingSpaces[i] - nestDepths[nestDepths.length - 1][1] < 2) {
-        result += `</li>\n</${nestDepths[nestDepths.length - 1][0]}>`;
-        nestDepths.pop();
-      }
-
-      result += `</li>\n<li ${classBox}>${input} ${line.replace(reg, '').trim()}`;
+      result += `\n<${el} ${lineNum}>\n<li ${classBox}>${input} ${line
+        .replace(markDownRegex, '')
+        .trim()}`;
+      return;
     }
+
+    // pushIn < 0
+    // nestOut
+    // pop out of nest as many times as we need to
+    while (nestDepths.length > 1 && leadingSpaces[i] - nestDepths[nestDepths.length - 1][1] < 2) {
+      result += `</li>\n</${nestDepths[nestDepths.length - 1][0]}>`;
+      nestDepths.pop();
+    }
+
+    result += `</li>\n<li ${classBox}>${input} ${line.replace(markDownRegex, '').trim()}`;
   });
 
   // close all open nests
